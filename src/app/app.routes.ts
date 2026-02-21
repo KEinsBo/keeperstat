77bfdbc2b314972a853029b4bdc8575cbd20baa5
KEinsBo <mattis-richter@gmx.de>
2026-02-21 13:28:43 +0100

    Created the basic design and structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Main } from './pages/main/main';
+import { Settings } from './pages/settings/settings';
+import { Stats } from './pages/stats/stats';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'main', component: Main },
+  { path: 'settings', component: Settings },
+  { path: 'stats', component: Stats }
+];
